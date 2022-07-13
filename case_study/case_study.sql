10e0fffa77ac5f34ad378fa600fc3eca8ebc58cd
dntrung1 <dntrung1@gmail.com>
2022-07-13 15:24:27 +0700

    update

---
+++ case_study/case_study.sql
@@ -0,0 +1,108 @@
+create database furama;
+
+use furama;
+
+create table hop_dong(
+	ma_hop_dong int primary key,
+	ngay_lam_hop_dong datetime not null,
+	ngay_ket_thuc datetime not null,
+	tien_dat_coc double not null,
+	ma_nhan_vien int not null,
+	ma_khach_hang int not null,
+	ma_dich_vu int not null
+);
+
+create table nhan_vien(
+	ma_nhan_vien int primary key,
+    ho_ten varchar(45) not null,
+    ngay_sinh date not null,
+    so_cmnd varchar(45) not null,
+    luong double not null,
+    so_dien_thoai varchar(45) not null,
+    emai varchar(45),
+	dia_chi varchar(45),
+    ma_vi_tri int not null,
+    ma_trinh_do int not null,
+    ma_bo_phan int not null
+);
+
+create table vi_tri(
+	ma_vi_tri int primary key,
+    ten_vi_tri varchar(45)
+);
+
+create table trinh_do(
+	ma_trinh_do int primary key,
+    ten_trinh_do varchar(45)
+);
+
+create table bo_phan(
+	ma_bo_phan int primary key,
+    ten_bo_phan varchar(45)
+);
+
+create table khach_hang(
+	ma_khach_hang int primary key,
+    ma_loai_khach int,
+	ho_ten varchar(45) not null,
+    ngay_sinh date not null,
+    gioi_tinh bit(1) not null,
+    so_cmnd varchar(45) not null,
+    so_dien_thoai varchar(45) not null,
+    emai varchar(45),
+	dia_chi varchar(45)
+);
+
+create table loai_khach(
+	ma_loai_khach int primary key,
+    ten_loai_khach varchar(45)
+);
+
+create table dich_vu(
+	ma_dich_vu int primary key,
+    ten_dich_vu varchar(45) not null,
+    dien_tich int,
+    chi_phi_thue double not null,
+    so_nguoi_toi_Da int,
+    ma_kieu_thue int not null,
+    ma_loai_dich_vu int not null,
+    tieu_chuan_phong varchar(45),
+    mo_ta_tien_nghi_khac varchar(45),
+    dien_tich_ho_boi double,
+    so_tang int,
+    dich_vu_mien_phi_di_kem text
+);
+
+create table hop_dong_chi_tiet(
+	ma_hop_dong_chi_tiet int primary key,
+    ma_hop_dong int not null,
+    ma_dich_vu_di_kem int not null,
+    so_luong int not null
+);
+
+create table dich_vu_di_kem(
+	ma_dich_vu_di_kem int primary key,
+    ten_dich_vu_di_kem varchar(45) not null,
+    gia double not null,
+    don_vi varchar(10) not null,
+    trang_thai varchar(45)
+);
+
+create table loai_dich_vu(
+	ma_loai_dich_vu int primary key,
+    ten_loai_dich_vu varchar(45)
+);
+
+create table kieu_thue(
+	ma_kieu_thue int primary key,
+    ten_kieu_thue varchar(45)
+);
+
+
+
+
+
+
+
+
+
--- ss1_Database/exercise/xay_dung_csdl.sql
+++ ss1_Database/exercise/xay_dung_csdl.sql
@@ -1,5 +1,5 @@
-create database xay_dung_csdl;
-use xay_dung_csdl;
+create database student_management;
+use student_management;
 create table class(
 id_class int auto_increment,
 name_class varchar(50),
@@ -18,4 +18,6 @@ insert into class(name_class) values ("C0422G1"),("C0322G1");
 
 insert into teacher(name_student,age_student,country_student) values ("Trung",18,"da nang"),("Trong",38,"quang nam");
 
+select * from class;
 
+select * from teacher;
