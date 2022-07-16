9ccf1d296fa1a0d80274dcc1bf2e9d7994174c1b
dntrung1 <dntrung1@gmail.com>
2022-07-16 08:18:03 +0700

    update

--- case_study/case_study.sql
+++ case_study/case_study.sql
@@ -4,114 +4,125 @@ create database furama;
 
 use furama;
 
-create table vi_tri(
-	ma_vi_tri int primary key,
-    ten_vi_tri varchar(45)
+CREATE TABLE vi_tri (
+    ma_vi_tri INT PRIMARY KEY,
+    ten_vi_tri VARCHAR(45)
 );
 
-create table trinh_do(
-	ma_trinh_do int primary key,
-    ten_trinh_do varchar(45)
+CREATE TABLE trinh_do (
+    ma_trinh_do INT PRIMARY KEY,
+    ten_trinh_do VARCHAR(45)
 );
 
-create table bo_phan(
-	ma_bo_phan int primary key,
-    ten_bo_phan varchar(45)
+CREATE TABLE bo_phan (
+    ma_bo_phan INT PRIMARY KEY,
+    ten_bo_phan VARCHAR(45)
 );
 
-create table loai_khach(
-	ma_loai_khach int primary key,
-    ten_loai_khach varchar(45)
+CREATE TABLE loai_khach (
+    ma_loai_khach INT PRIMARY KEY,
+    ten_loai_khach VARCHAR(45)
 );
 
-create table loai_dich_vu(
-	ma_loai_dich_vu int primary key,
-    ten_loai_dich_vu varchar(45)
+CREATE TABLE loai_dich_vu (
+    ma_loai_dich_vu INT PRIMARY KEY,
+    ten_loai_dich_vu VARCHAR(45)
 );
 
-create table kieu_thue(
-	ma_kieu_thue int primary key,
-    ten_kieu_thue varchar(45)
+CREATE TABLE kieu_thue (
+    ma_kieu_thue INT PRIMARY KEY,
+    ten_kieu_thue VARCHAR(45)
 );
 
-create table dich_vu_di_kem(
-	ma_dich_vu_di_kem int primary key,
-    ten_dich_vu_di_kem varchar(45) not null,
-    gia double not null,
-    don_vi varchar(10) not null,
-    trang_thai varchar(45)
+CREATE TABLE dich_vu_di_kem (
+    ma_dich_vu_di_kem INT PRIMARY KEY,
+    ten_dich_vu_di_kem VARCHAR(45) NOT NULL,
+    gia DOUBLE NOT NULL,
+    don_vi VARCHAR(10) NOT NULL,
+    trang_thai VARCHAR(45)
 );
 
-create table nhan_vien(
-	ma_nhan_vien int primary key,
-    ho_ten varchar(45) not null,
-    ngay_sinh date not null,
-    so_cmnd varchar(45) not null,
-    luong double not null,
-    so_dien_thoai varchar(45) not null,
-    emai varchar(45),
-	dia_chi varchar(45),
-    ma_vi_tri int not null,
-    ma_trinh_do int not null,
-    ma_bo_phan int not null,
-    foreign key(ma_vi_tri) references vi_tri(ma_vi_tri),
-    foreign key(ma_trinh_do) references trinh_do(ma_trinh_do),
-    foreign key(ma_bo_phan) references bo_phan(ma_bo_phan)
+CREATE TABLE nhan_vien (
+    ma_nhan_vien INT PRIMARY KEY,
+    ho_ten VARCHAR(45) NOT NULL,
+    ngay_sinh DATE NOT NULL,
+    so_cmnd VARCHAR(45) NOT NULL,
+    luong DOUBLE NOT NULL,
+    so_dien_thoai VARCHAR(45) NOT NULL,
+    emai VARCHAR(45),
+    dia_chi VARCHAR(45),
+    ma_vi_tri INT NOT NULL,
+    ma_trinh_do INT NOT NULL,
+    ma_bo_phan INT NOT NULL,
+    FOREIGN KEY (ma_vi_tri)
+        REFERENCES vi_tri (ma_vi_tri),
+    FOREIGN KEY (ma_trinh_do)
+        REFERENCES trinh_do (ma_trinh_do),
+    FOREIGN KEY (ma_bo_phan)
+        REFERENCES bo_phan (ma_bo_phan)
 );
 
-create table khach_hang(
-	ma_khach_hang int primary key,
-    ma_loai_khach int,
-    foreign key(ma_loai_khach)references loai_khach(ma_loai_khach),
-	ho_ten varchar(45) not null,
-    ngay_sinh date not null,
-    gioi_tinh bit(1) not null,
-    so_cmnd varchar(45) not null,
-    so_dien_thoai varchar(45) not null,
-    emai varchar(45),
-	dia_chi varchar(45)
-);
-
-
-
-create table dich_vu(
-	ma_dich_vu int primary key,
-    ten_dich_vu varchar(45) not null,
-    dien_tich int,
-    chi_phi_thue double not null,
-    so_nguoi_toi_Da int,
-    ma_kieu_thue int not null,
-    foreign key(ma_kieu_thue)references kieu_thue(ma_kieu_thue),
-    ma_loai_dich_vu int not null,
-    foreign key(ma_loai_dich_vu) references loai_dich_vu(ma_loai_dich_vu),
-    tieu_chuan_phong varchar(45),
-    mo_ta_tien_nghi_khac varchar(45),
-    dien_tich_ho_boi double,
-    so_tang int,
-    dich_vu_mien_phi_di_kem text
+CREATE TABLE khach_hang (
+    ma_khach_hang INT PRIMARY KEY,
+    ma_loai_khach INT,
+    FOREIGN KEY (ma_loai_khach)
+        REFERENCES loai_khach (ma_loai_khach),
+    ho_ten VARCHAR(45) NOT NULL,
+    ngay_sinh DATE NOT NULL,
+    gioi_tinh BIT(1) NOT NULL,
+    so_cmnd VARCHAR(45) NOT NULL,
+    so_dien_thoai VARCHAR(45) NOT NULL,
+    emai VARCHAR(45),
+    dia_chi VARCHAR(45)
 );
 
 
-create table hop_dong(
-	ma_hop_dong int primary key,
-	ngay_lam_hop_dong datetime not null,
-	ngay_ket_thuc datetime not null,
-	tien_dat_coc double not null,
-	ma_nhan_vien int not null,
-	ma_khach_hang int not null,
-	ma_dich_vu int not null,
-    foreign key(ma_nhan_vien) references nhan_vien(ma_nhan_vien),
-    foreign key(ma_khach_hang) references khach_hang(ma_khach_hang),
-    foreign key(ma_dich_vu) references dich_vu(ma_dich_vu)
+
+CREATE TABLE dich_vu (
+    ma_dich_vu INT PRIMARY KEY,
+    ten_dich_vu VARCHAR(45) NOT NULL,
+    dien_tich INT,
+    chi_phi_thue DOUBLE NOT NULL,
+    so_nguoi_toi_Da INT,
+    ma_kieu_thue INT NOT NULL,
+    FOREIGN KEY (ma_kieu_thue)
+        REFERENCES kieu_thue (ma_kieu_thue),
+    ma_loai_dich_vu INT NOT NULL,
+    FOREIGN KEY (ma_loai_dich_vu)
+        REFERENCES loai_dich_vu (ma_loai_dich_vu),
+    tieu_chuan_phong VARCHAR(45),
+    mo_ta_tien_nghi_khac VARCHAR(45),
+    dien_tich_ho_boi DOUBLE,
+    so_tang INT,
+    dich_vu_mien_phi_di_kem TEXT
 );
 
-create table hop_dong_chi_tiet(
-	ma_hop_dong_chi_tiet int primary key,
-    ma_hop_dong int not null,
-    foreign key(ma_hop_dong) references hop_dong(ma_hop_dong),
-    ma_dich_vu_di_kem int not null,
-    foreign key(ma_dich_vu_di_kem) references dich_vu_di_kem(ma_dich_vu_di_kem),
-    so_luong int not null
+
+CREATE TABLE hop_dong (
+    ma_hop_dong INT PRIMARY KEY,
+    ngay_lam_hop_dong DATETIME NOT NULL,
+    ngay_ket_thuc DATETIME NOT NULL,
+    tien_dat_coc DOUBLE NOT NULL,
+    ma_nhan_vien INT NOT NULL,
+    ma_khach_hang INT NOT NULL,
+    ma_dich_vu INT NOT NULL,
+    FOREIGN KEY (ma_nhan_vien)
+        REFERENCES nhan_vien (ma_nhan_vien),
+    FOREIGN KEY (ma_khach_hang)
+        REFERENCES khach_hang (ma_khach_hang),
+    FOREIGN KEY (ma_dich_vu)
+        REFERENCES dich_vu (ma_dich_vu)
+);
+
+CREATE TABLE hop_dong_chi_tiet (
+    ma_hop_dong_chi_tiet INT PRIMARY KEY,
+    ma_hop_dong INT NOT NULL,
+    FOREIGN KEY (ma_hop_dong)
+        REFERENCES hop_dong (ma_hop_dong),
+    ma_dich_vu_di_kem INT NOT NULL,
+    FOREIGN KEY (ma_dich_vu_di_kem)
+        REFERENCES dich_vu_di_kem (ma_dich_vu_di_kem),
+    so_luong INT NOT NULL
 );
 
 INSERT INTO furama.vi_tri (ma_vi_tri, ten_vi_tri) 
@@ -153,22 +164,61 @@ VALUES ('1', 'Diamond'),
  ('4', 'Silver'),
  ('5', 'Member');
  
+  INSERT INTO furama.khach_hang  (ma_khach_hang, ho_ten, ngay_sinh, gioi_tinh, so_cmnd, so_dien_thoai, emai, dia_chi, ma_loai_khach) 
+VALUES ('1', 'Nguyễn Thị Hào', '1970-11-07', 0, '643431213', '0945423362', 'thihao07@gmail.com', '23 Nguyễn Hoàng, Đà Nẵng', '5'),
+ ('2', 'Phạm Xuân Diệu', '1992-08-08', 1, '865342123', '0954333333', 'xuandieu92@gmail.com', 'K77/22 Thái Phiên, Quảng Trị', '3'),
+ ('3', 'Trương Đình Nghệ', '1990-02-27', 1, '488645199', '0373213122', 'nghenhan2702@gmail.com', 'K323/12 Ông Ích Khiêm, Vinh', '1'),
+ ('4', 'Dương Văn Quan', '1981-07-08', 1, '543432111', '0490039241', 'duongquan@gmail.com', 'K453/12 Lê Lợi, Đà Nẵng', '1'),
+ ('5', 'Hoàng Trần Nhi Nhi', '1995-12-09', 0, '795453345', '0312345678', 'nhinhi123@gmail.com', '224 Lý Thái Tổ, Gia Lai', '4'),
+ ('6', 'Tôn Nữ Mộc Châu', '2005-12-06', 0, '732434215', '0988888844', 'tonnuchau@gmail.com', '37 Yên Thế, Đà Nẵng', '4'),
+ ('7', 'Nguyễn Mỹ Kim', '1984-04-08', 0, '856453123', '0912345698', 'kimcuong84@gmail.com', 'K123/45 Lê Lợi, Hồ Chí Minh', '1'),
+ ('8', 'Nguyễn Thị Hào', '1999-04-08', 0, '965656433', '0763212345', 'haohao99@gmail.com', '55 Nguyễn Văn Linh, Kon Tum', '3'),
+ ('9', 'Trần Đại Danh', '1994-07-01', 1, '432341235', '0643343433', 'danhhai99@gmail.com', '24 Lý Thường Kiệt, Quảng Ngãi', '1'),
+ ('10', 'Nguyễn Tâm Đắc', '1989-07-01', 1, '344343432', '0987654321', 'dactam@gmail.com', '22 Ngô Quyền, Đà Nẵng', '2');
 
+insert into kieu_thue 
+values (1,"year"),(2,"month"),(3,"day"),(4,"hour");
 
+ insert into loai_dich_vu
+ values (1,'villa'), (2,'house'),(3,'room');
 
+insert into dich_vu (ma_dich_vu,ten_dich_vu,dien_tich,chi_phi_thue,so_nguoi_toi_da,tieu_chuan_phong,mo_ta_tien_nghi_khac,dien_tich_ho_boi,so_tang,ma_kieu_thue,ma_loai_dich_vu)
+values (1,'Villa Beach Front',25000,1000000,10,'vip','Có hồ bơi',500,4,3,1),
+(4,'Villa No Beach Front',22000,9000000,8,'normal','Có hồ bơi',300,4,3,1);
+insert into dich_vu (ma_dich_vu,ten_dich_vu,dien_tich,chi_phi_thue,so_nguoi_toi_da,tieu_chuan_phong,mo_ta_tien_nghi_khac,so_tang,ma_kieu_thue,ma_loai_dich_vu)
+value (2,'House Princess 01',14000,5000000,7,'vip','Có thêm bếp nướng',3,2,2),
+(5,'House Princess 02',10000,4000000,5,'normal','Có thêm bếp nướng',2,2,2);
+insert into dich_vu (ma_dich_vu,ten_dich_vu,dien_tich,chi_phi_thue,so_nguoi_toi_da,tieu_chuan_phong,mo_ta_tien_nghi_khac,dich_vu_mien_phi_di_kem,ma_kieu_thue,ma_loai_dich_vu)
+values (3,'Room Twin 01',5000,1000000,2,'normal','Có tivi','1 Xe máy, 1 Xe đạp',4,3),
+(6,'Room Twin 02',3000,900000,2,'normal','Có tivi','1 Xe máy',4,3);
 
+insert into dich_vu_di_kem
+values(1,'Karaoke',10000,'giờ','tiện nghi, hiện tại'),
+(2,'Thuê xe máy',10000,'chiếc','hỏng 1 xe'),
+(3,'Thuê xe đạp',20000,'chiếc','tốt'),
+(4,'Buffet buổi sáng',15000,'suất','đầy đủ đồ ăn, tráng miệng'),
+(5,'Buffet buổi trưa',90000,'suất','đầy đủ đồ ăn, tráng miệng'),
+(6,'Buffet buổi tối',16000,'suất','đầy đủ đồ ăn, tráng miệng');
 
+INSERT INTO hop_dong (ma_hop_dong, ngay_lam_hop_dong, ngay_ket_thuc, tien_dat_coc, ma_nhan_vien, ma_khach_hang, ma_dich_vu) 
+VALUES  (1, '2020-12-08', '2020-12-08', 0, 3, 1, 3),
+  (2, '2020-07-14', '2020-07-21', 200000, 7, 3, 1),
+  (3, '2021-03-15', '2021-03-17', 50000, 3, 4, 2),
+  (4, '2021-01-14', '2021-01-18', 100000, 7, 5, 5),
+  (5, '2021-07-14', '2021-07-15', 0, 7, 2, 6),
+  (6, '2021-06-01', '2021-06-03', 0, 7, 7, 6),
+  (7, '2021-09-02', '2021-09-05', 100000, 7, 4, 4),
+  (8, '2021-06-17', '2021-06-18', 150000, 3, 4, 1),
+  (9, '2020-11-19', '2020-11-19', 0, 3, 4, 3),
+  (10, '2021-04-12', '2021-04-14', 0, 10, 3, 5),
+  (11, '2021-04-25', '2021-04-25', 0, 2, 2, 1),
+  (12, '2021-05-25', '2021-05-27', 0, 7, 10, 1);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+insert into hop_dong_chi_tiet
+values (1,2,4,5),
+(2,2,5,8),
+(3,2,6,15),
+(4,3,1,1),
+(5,3,2,11),
+(6,1,3,1),
+(8,12,2,2);
